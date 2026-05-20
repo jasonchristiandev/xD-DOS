7d77619046d8929a7207a39d6b01ee42727ac154
jasonchristiandev <jasonchristiandev@gmail.com>
2026-05-20 08:38:06 +0700

    fixed requests.h

--- include/xD-DOS/requests.h
+++ include/xD-DOS/requests.h
@@ -14,25 +14,6 @@
 #define XD_DOS_MEMMAP_FRAMEBUFFER 7
 #define XD_DOS_MEMMAP_RESERVED_MAPPED 8
 
-__attribute__((used, section(".limine_requests"))) static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
-__attribute__((used, section(".limine_requests_start"))) static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
-__attribute__((used, section(".limine_requests_end"))) static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
-__attribute__((used, section(".limine_requests"))) static volatile struct limine_executable_address_request executable_address_request = {
-	.id = LIMINE_EXECUTABLE_ADDRESS_REQUEST_ID,
-	.revision = 0};
-__attribute__((used, section(".limine_requests"))) static volatile struct limine_executable_file_request executable_file_request = {
-	.id = LIMINE_EXECUTABLE_FILE_REQUEST_ID,
-	.revision = 0};
-__attribute__((used, section(".limine_requests"))) static volatile struct limine_memmap_request memmap_request = {
-	.id = LIMINE_MEMMAP_REQUEST_ID,
-	.revision = 0};
-__attribute__((used, section(".limine_requests"))) static volatile struct limine_hhdm_request hhdm_request = {
-	.id = LIMINE_HHDM_REQUEST_ID,
-	.revision = 0};
-__attribute__((used, section(".limine_requests"))) static volatile struct limine_framebuffer_request framebuffer_request = {
-	.id = LIMINE_FRAMEBUFFER_REQUEST_ID,
-	.revision = 0};
-
 typedef struct {
 	uint64_t phys;
 	uint64_t virt;
--- src/requests.c
+++ src/requests.c
@@ -3,14 +3,24 @@
 #include <stddef.h>
 #include <stdint.h>
 
-extern volatile uint64_t limine_base_revision[];
-extern volatile uint64_t limine_requests_start_marker[];
-extern volatile uint64_t limine_requests_end_marker[];
-extern volatile struct limine_executable_address_request executable_address_request;
-extern volatile struct limine_executable_file_request executable_file_request;
-extern volatile struct limine_memmap_request memmap_request;
-extern volatile struct limine_hhdm_request hhdm_request;
-extern volatile struct limine_framebuffer_request framebuffer_request;
+__attribute__((used, section(".limine_requests"))) static volatile uint64_t limine_base_revision[] = LIMINE_BASE_REVISION(6);
+__attribute__((used, section(".limine_requests_start"))) static volatile uint64_t limine_requests_start_marker[] = LIMINE_REQUESTS_START_MARKER;
+__attribute__((used, section(".limine_requests_end"))) static volatile uint64_t limine_requests_end_marker[] = LIMINE_REQUESTS_END_MARKER;
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_executable_address_request executable_address_request = {
+	.id = LIMINE_EXECUTABLE_ADDRESS_REQUEST_ID,
+	.revision = 0};
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_executable_file_request executable_file_request = {
+	.id = LIMINE_EXECUTABLE_FILE_REQUEST_ID,
+	.revision = 0};
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_memmap_request memmap_request = {
+	.id = LIMINE_MEMMAP_REQUEST_ID,
+	.revision = 0};
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_hhdm_request hhdm_request = {
+	.id = LIMINE_HHDM_REQUEST_ID,
+	.revision = 0};
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_framebuffer_request framebuffer_request = {
+	.id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+	.revision = 0};
 
 uint8_t request_base_revision_supported() {
 	return LIMINE_BASE_REVISION_SUPPORTED(limine_base_revision);
